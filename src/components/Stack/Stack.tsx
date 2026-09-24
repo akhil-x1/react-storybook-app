a62cc24a60f8d6a4227d08a50450e84f7e6d5b51
akhil <akhil@geekyants.com>
2026-09-24 17:18:52 +0530

    Updated stack.tsx -  added interface stackProps. Added default values.

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,13 +1,26 @@
+import { ReactNode } from 'react';
 import PropTypes from 'prop-types';
 
-export default function Stack({ children, wrap = false }) {
+interface StackProps {
+  children?: ReactNode;
+  direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  wrap?: boolean;
+  spacing?: number;
+}
+
+export default function Stack({
+  children,
+  direction = 'row',
+  wrap = false,
+  spacing = 2,
+}: StackProps) {
   return (
     <div
       style={{
         display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        gap: '0.25rem',
+        flexDirection: direction,
+        flexWrap: wrap ? 'wrap' : 'nowrap',
+        gap: `${spacing * 0.25}rem`,
       }}
     >
       {children}
